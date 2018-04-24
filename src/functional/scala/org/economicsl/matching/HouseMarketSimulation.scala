ee6630912bf43643bf13289e302947fc77c5102a
davidrpugh <david.pugh@kapsarc.org>
2018-04-24 07:46:17 +0300

    Added License information.

--- src/functional/scala/org/economicsl/matching/HouseMarketSimulation.scala
+++ src/functional/scala/org/economicsl/matching/HouseMarketSimulation.scala
@@ -1,3 +1,18 @@
+/*
+Copyright 2018 EconomicSL
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+   http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
 package org.economicsl.matching
 
 import org.economicsl.core.Price
@@ -8,18 +23,18 @@ import scala.collection.immutable.HashSet
 
 object HouseMarketSimulation extends App {
 
-  // define some generators for the input data
+  // this generator should exist in esl-core!
+  val priceGen: Gen[Price] = {
+    for {
+      value <- Gen.choose(100000, 1000000)
+    } yield Price(value)
+  }
+
   val houseGen: Gen[House] = {
     for {
       id <- Gen.posNum[Long]
       quality <- Gen.choose(1, 100)
     } yield House(id, quality)
-  }
-
-  val priceGen: Gen[Price] = {
-    for {
-      value <- Gen.choose(100000, 1000000)
-    } yield Price(value)
   }
 
   val houseListingGen: Gen[HouseListing] = {
@@ -33,7 +48,7 @@ object HouseMarketSimulation extends App {
 
   val housePurchaseOfferGen: Gen[HousePurchaseOffer] = {
     for {
-      id <- Gen.choose(0L, Long.MaxValue)
+      id <- Gen.posNum[Long]
       issuer <- Gen.posNum[Long]
       price <- priceGen
     } yield HousePurchaseOffer(id, issuer, price)
