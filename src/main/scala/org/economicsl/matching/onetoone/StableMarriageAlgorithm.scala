97109e37439857d8d543d7754380c08dee4a2534
davidrpugh <david.pugh@kapsarc.org>
2018-05-04 20:56:13 +0300

    Added separate packages for manytoone and onetoone matchings.

--- src/main/scala/org/economicsl/matching/onetoone/StableMarriageAlgorithm.scala
+++ src/main/scala/org/economicsl/matching/onetoone/StableMarriageAlgorithm.scala
@@ -13,8 +13,9 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
 */
-package org.economicsl.matching
+package org.economicsl.matching.one2one
 
+import org.economicsl.matching.{Preferences, Proposer}
 import org.scalactic.TripleEquals._
 
 
